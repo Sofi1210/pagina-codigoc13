e22d3eae85c1f462151e7e5b8499cfa641677af6
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2022-11-25 10:53:54 -0500

    Creamos iniciar sesión

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,17 +1,11 @@
 import React, { useContext, useState } from "react";
 import { themeContext } from "../../App";
 import "./header.css";
-import SignIn from '../regist/InicioSession'
-import { blue } from '@mui/material/colors';
-import Avatar from "@mui/material/Avatar";
 import Switch from "@mui/material/Switch";
-import PersonIcon from '@mui/icons-material/Person';
-import Swal from "sweetalert2";
 import News from "../news/News";
 import We from "../we/AboutUS"
 import Careers from "../Careers/Careers";
-
-
+import SignUp from "../SignUp";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
@@ -23,31 +17,14 @@ const Header = () => {
     setTheme(theme === "dark" ? "light" : "dark");
     setLogo(theme === "dark" ? "logo-light" : "logo-dark");
   };
-
-  const showAlert = () =>{
-
-    const { value: email } = Swal.fire({
-      html:
-      ` ${SignIn}`
-    });
-    
-    if (email) {
-
-      Swal.fire(`Entered email: ${email}`)
-    }
-  }
-
-
+  
   return (
     <div>
       <section className="header">
 
         <div className="menuTop">
 
-          <Avatar className="session" sx={{ bgcolor: blue[500] }} onClick={showAlert}>
-            <PersonIcon/>
-          </Avatar>
-          
+          <SignUp/>
 
           <div id={logo} className="logo">
 
---
+++ src/Page/Carrers/CarrersView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CarrersView = () => {
+  return (
+    <div>CarrersView</div>
+  )
+}
+
+export default CarrersView
